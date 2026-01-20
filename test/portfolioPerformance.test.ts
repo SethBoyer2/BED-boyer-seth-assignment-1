311c5b886b9c747630bf0dfa1b93c9bc241d027d
Seth <sboyer2@academic.rrc.ca>
2026-01-19 18:47:50 -0600

    Feat: Partially completed one integration and one unit test - need to fix bugs.

---
+++ test/portfolioPerformance.test.ts
@@ -0,0 +1,25 @@
+import request, { Response } from "supertest";
+import app from "../src/app";
+import { calculatePortfolioPerformance } from "../src/portfolio/portfolioPerformance";
+
+describe("GET api/v1/performance", () => {
+  it("should return all fields with valid information", async () => {
+    const response: Response = await request(app).get("/api/v1/performance?initialInvestment=10000&currentValue=12000");
+    expect(response.body).toEqual
+      ({
+        initialInvestment: 10000,
+        currentValue: 12000,
+        profitOrLoss: 2000,
+        percentageChange: 20,
+        performanceSummary: "The portfolio has performed poorly."
+      });
+  });
+
+  it("should return profit or loss calculation successfully", async () => {
+    const testCase1 = calculatePortfolioPerformance(10000, 20000)
+
+    expect(testCase1.profitOrLoss).toEqual(10000)
+  });
+
+});
+
